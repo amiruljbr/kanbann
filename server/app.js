76977e66b9aa2644aff5393c673740944c9d97b5
amiruljbr <amiruljabar@gmail.com>
2020-06-19 20:21:26 +0700

    config database

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const router = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
